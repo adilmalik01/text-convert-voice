2873e1649a4a2d1b1037a5be0a77b56bdc9811ec
adil5679 <malikadil5679@gmail.com>
2024-01-31 20:20:58 +0500

    js done

---
+++ app.js
@@ -0,0 +1,34 @@
+let userInp = document.querySelector("#userText")
+let listenBtn = document.querySelector(".listenBtn")
+let pauseBtn = document.querySelector(".pauseBtn")
+let voicesSelect = document.querySelector(".lang")
+
+let msg = new SpeechSynthesisUtterance;
+
+let voices = []
+
+window.speechSynthesis.onvoiceschanged = (e) => {
+    voices = window.speechSynthesis.getVoices()
+    msg.voice = voices[0]
+
+    voices.forEach((voice, index) => {
+        voicesSelect.options[index] = new Option(voice.name, index)
+        console.log(voicesSelect.options[index], voice.name, index);
+    })
+}
+
+voicesSelect.addEventListener("change", (e) => {
+    msg.voice = voices[voicesSelect.value]
+    console.log(voicesSelect.value);
+})
+
+
+const listenVoice = (e) => {
+    msg.text = userInp.value;
+    window.speechSynthesis.speak(msg)
+    console.log('funtion is still working', userInp.value);
+}
+
+
+listenBtn.addEventListener("click", listenVoice)
+// pauseBtn.addEventListener("click", pauseBtnVoice)
